5ed3cbb866373a980a32544c8960c01ec36a2a62
Acharya Anil <thakurinfotech@gmail.com>
2018-07-17 10:33:03 +0530

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  chooselan = ""
+  constructor(public translate: TranslateService){
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('hi');
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use('hi');
+  }
+
+
+  changeLang(lan){
+    console.log(lan,'lan')
+    this.translate.use(lan);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule, HttpClient} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {TranslateModule,TranslateLoader} from "@ngx-translate/core";
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {FormsModule} from "@angular/forms";
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+      }
+
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
